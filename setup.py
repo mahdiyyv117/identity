87dbd07d5f8d8ff20c9e2fed2ae0071298a61e6b
mmdbalkhi <ahmdparsh129@gmail.com>
2021-10-16 14:06:35 +0330

    change readme type to md

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
 from setuptools import setup
 
-setup(name='identity', version='1.0',
-      py_modules=['identity'],
-      author_email='')
+setup(name="identity", version="1.0", py_modules=["identity"], author_email="")
