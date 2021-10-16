d612d0f121b086d9cddc76105b58a1f6dc79e501
mmdbalkhi <ahmdparsh129@gmail.com>
2021-10-16 14:08:03 +0330

    fix setup.py and add lib description to file

--- setup.py
+++ setup.py
@@ -1,3 +1,31 @@
-from setuptools import setup
+def setup_package():
+    metadata = dict(
+        name="identity",
+        packages=["identity"],
+        version="1.0",
 
-setup(name="identity", version="1.0", py_modules=["identity"], author_email="")
+        description="It is a small library for solving some mathematical alliances and some mathematical instructions such as factorial",
+        author="mahdiyavari",
+        author_email="mahdiyyv@gmail.com",
+        url="https://github.com/mahdiyyv117/identity",
+        keywords=[
+            "اتحاد",
+            "identity",
+
+        ],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+        ],
+        python_requires=">=3.6",
+    )
+    try:
+        from setuptools import setup
+    except ImportError:
+        from distutils.core import setup
+
+    setup(**metadata)
+
+
+if __name__ == "__main__":
+    setup_package()
