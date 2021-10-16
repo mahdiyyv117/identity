a487d3256bb314283c6c22ba602f1b7029515d99
mmdbalkhi <ahmdparsh129@gmail.com>
2021-10-16 13:57:38 +0330

    fix bugs and Lint the codes

--- identity.py
+++ identity.py
@@ -1,252 +1,202 @@
+"""It is a small library for solving some mathematical alliances
+and some mathematical instructions such as factorial."""
+
+
 def factorial(n: int) -> int:
-    '''of factorial for computing
-    multiplication 1 until the n use becomes'''
+    """of factorial for computing
+    multiplication 1 until the n use becomes"""
     if not isinstance(n, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     elif n < 0:
-        raise ValueError('this function for number \
-                         negative definition not becomes')
-        return None
+        raise ValueError(
+            "this function for number \
+                         negative definition not becomes"
+        )
     elif n == 0:
         return 1
     else:
-        factorial1 = n * factorial(n-1)
-        return factorial1
+        return n * factorial(n - 1)
 
 
 def isq(a: int, b: int) -> int:
-    '''(a+b)^2=a^2+2ab+b^2'''
+    """(a+b)^2=a^2+2ab+b^2"""
     if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     else:
-        Isq = (a+b)**2
-        return Isq
+        return (a + b) ** 2
 
 
 def iisq(a: int, b: int) -> int:
-    '''(a-b)^2=a^2-2ab+b^2'''
+    """(a-b)^2=a^2-2ab+b^2"""
     if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     else:
-        IIsq = (a-b)**2
-        return IIsq
+        return (a - b) ** 2
 
 
 def square_3(a: int, b: int, c: int) -> int:
-    '''(a+b+c)^2=a^2+b^2+c^2+2ab+2ac+2bc'''
-    if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
-    if not isinstance(c, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+    """(a+b+c)^2=a^2+b^2+c^2+2ab+2ac+2bc"""
+    if not isinstance(a, int) or not isinstance(b, int) or not isinstance(c, int):
+        raise TypeError("this function only for int definition becomes")
+
     else:
-        square_3 = (a+b+c)**2
-        return square_3
+        return (a + b + c) ** 2
 
 
 def square_3_neg(a: int, b: int, c: int) -> int:
-    '''(a+b-c)^2=a^2+b^2+c^2+2ab-2ac-2bc'''
-    if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
-    if not isinstance(c, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+    """(a+b-c)^2=a^2+b^2+c^2+2ab-2ac-2bc"""
+    if not isinstance(a, int) or not isinstance(b, int) or not isinstance(c, int):
+        raise TypeError("this function only for int definition becomes")
     else:
-        square_3_neg = (a+b-c)**2
-        return square_3_neg
+        return (a + b - c) ** 2
 
 
 def opshn_s(*argus: int) -> int:
-    '''optional identity square'''
+    """optional identity square"""
     for arg in argus:
         if not isinstance(arg, int):
-            raise TypeError('this function only for int definition becomes')
-            return None
+            raise TypeError("this function only for int definition becomes")
     else:
-        opshn_s = sum(argus)**2
-        return opshn_s
+        return sum(argus) ** 2
 
 
-def opshn_s_neg(endneg: int, *argus: int) -> int:
-    '''optional identity square negative'''
+def opshn_s_neg(*argus: int, endneg: int) -> int:
+    """optional identity square negative"""
     for arg in argus:
         if not isinstance(arg, int) or not isinstance(endneg, int):
-            raise TypeError('this function only for int definition becomes')
-            return None
+            raise TypeError("this function only for int definition becomes")
     else:
-        sumarg = sum(argus)
-        opshn_s_neg = (sumarg-endneg)**2
-        return opshn_s_neg
+        return (sum(argus) - endneg) ** 2
 
 
 def cube(a: int, b: int) -> int:
-    '''(a+b)^3=a^3+3a^2b+3ab^2+b^3'''
+    """(a+b)^3=a^3+3a^2b+3ab^2+b^3"""
     if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     else:
-        cube = (a+b)**3
-        return cube
+        return (a + b) ** 3
 
 
 def cube_neg(a: int, b: int) -> int:
-    '''(a-b)^3=a^3-3a^2b+3ab^2-b^3'''
+    """(a-b)^3=a^3-3a^2b+3ab^2-b^3"""
     if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     else:
-        cube_neg = (a-b)**3
-        return cube_neg
+        return (a - b) ** 3
 
 
 def opshn_c(*argus: int) -> int:
-    '''optional identity cube'''
+    """optional identity cube"""
     for arg in argus:
         if not isinstance(arg, int):
-            raise TypeError('this function only for int definition becomes')
-            return None
+            raise TypeError("this function only for int definition becomes")
     else:
-        opshn_c = sum(argus)**3
-        return opshn_c
+        return sum(argus) ** 3
 
 
 def coupled(a: int, b: int) -> int:
-    '''(a+b)(a-b)=a^2-b^2'''
+    """(a+b)(a-b)=a^2-b^2"""
     if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     else:
-        coupled = a**2-b**2
-        return coupled
+        return a ** 2 - b ** 2
 
 
 def one_sentence_joint(x: int, a: int, b: int) -> int:
-    '''(x+a)(x+b)=x^2+(a+b)x+ab'''
+    """(x+a)(x+b)=x^2+(a+b)x+ab"""
     if not isinstance(x, int) or not isinstance(a, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     if not isinstance(b, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     else:
-        one_sentence_joint = (x+a)*(x+b)
-        return one_sentence_joint
+        return (x + a) * (x + b)
 
 
-def o_one_sentence_joint(x, *argus):
-    '''optional identity one sentence joint'''
+def o_one_sentence_joint(*argus, x):
+    """optional identity one sentence joint"""
     for arg in argus:
         if not isinstance(x, int) or not isinstance(arg, int):
-            raise TypeError('this function only for int definition becomes')
-            return None
+            raise TypeError("this function only for int definition becomes")
     else:
-        for o in list(argus):
-            o_one_sentence_joint = 1
-            o_one_sentence_joint *= (x+o)
-        return o_one_sentence_joint
+        i_one_sentence_joint = 1
+        for i in list(argus):
+            i_one_sentence_joint *= x + i
+        return i_one_sentence_joint
 
 
 def fatandthin(a: int, b: int) -> int:
-    '''a^3+b^3=(a+b)(a^2−ab+b^2)'''
+    """a^3+b^3=(a+b)(a^2−ab+b^2)"""
     if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     else:
-        fatandthin = a**3+b**3
-        return fatandthin
+        return a ** 3 + b ** 3
 
 
 def fatandthin_n(a: int, b: int) -> int:
-    '''a^3+b^3=(a-b)(a^2+ab+b^2)'''
+    """a^3+b^3=(a-b)(a^2+ab+b^2)"""
     if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     else:
-        fatandthin_n = a**3-b**3
-        return fatandthin_n
+        return a ** 3 - b ** 3
 
 
 def o_fatandthin(a: int, b: int, n: int) -> int:
-    '''optional identity fat and thin'''
+    """optional identity fat and thin"""
     if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     if not isinstance(n, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     elif n % 2 == 1:
-        print('power should odd number be')
-        return None
+        print("power should odd number be")
     else:
-        o_fatandthin = a**n+b**n
-        return o_fatandthin
+        return a ** n + b ** n
 
 
 def o_fatandthin_neg(a: int, b: int, n: int) -> int:
-    '''optional identity fat and thin negative'''
+    """optional identity fat and thin negative"""
     if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     if not isinstance(n, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     else:
-        o_fatandthin_neg = a**n-b**n
-        return o_fatandthin_neg
+        return a ** n - b ** n
 
 
 def lagrange(a: int, b: int, x: int, y: int) -> int:
-    '''(a^2+b^2)(x^2+y^2)=(ax+by)^2+(ay−bx)^2'''
+    """(a^2+b^2)(x^2+y^2)=(ax+by)^2+(ay−bx)^2"""
     if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     if not isinstance(x, int) or not isinstance(y, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     else:
-        lagrange = (a**2+b**2)*(x**2+y**2)
-        return lagrange
+        return (a ** 2 + b ** 2) * (x ** 2 + y ** 2)
 
 
 def avlir(a: int, b: int, c: int) -> int:
-    '''(a+b+c)(a^2+b^2+c^2−ab−ac−bc)=a^3+b^3+c^3−3abc'''
+    """(a+b+c)(a^2+b^2+c^2−ab−ac−bc)=a^3+b^3+c^3−3abc"""
     if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     if not isinstance(c, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     else:
-        avlir = a**3+b**3+c**3-3*a*b*c
-        return avlir
+        return a ** 3 + b ** 3 + c ** 3 - 3 * a * b * c
 
 
 def newton_two(a: int, b: int, n: int) -> int:
-    '''(a+b)^n=a^n+a^(n-1)b+a^(n-2)b^2+...+b^n'''
+    """(a+b)^n=a^n+a^(n-1)b+a^(n-2)b^2+...+b^n"""
     if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     if not isinstance(n, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     else:
-        newton_two = (a+b)**n
-        return newton_two
+        return (a + b) ** n
 
 
 def newton_two_n(a: int, b: int, n: int) -> int:
-    '''(a-b)^n=a^n-a^(n-1)b-a^(n-2)b^2-...-b^n'''
+    """(a-b)^n=a^n-a^(n-1)b-a^(n-2)b^2-...-b^n"""
     if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     if not isinstance(n, int):
-        raise TypeError('this function only for int definition becomes')
-        return None
+        raise TypeError("this function only for int definition becomes")
     else:
-        newton_two = (a-b)**n
-        return newton_two
+        return (a - b) ** n
